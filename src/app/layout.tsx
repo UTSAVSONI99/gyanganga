76a5b357ce0b87c04de28c144199cd00bbfbe874
Nikhil Kumar <nknapster@gmail.com>
2025-09-16 14:29:56 +0530

    changing to app dir

---
+++ src/app/layout.tsx
@@ -0,0 +1,35 @@
+  import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Green Agri Corp Pvt Ltd - Premium Indian Rice Exporter",
+  description: "Leading exporter of premium Indian non-basmati rice. State-of-the-art facilities ensuring highest quality grains. Trusted by leading brands worldwide.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+      {children}
+      </body>
+    </html>
+  );
+}
